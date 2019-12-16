7ad7e8abacada76d77e7c158b081f8f9e3cf8101
David DiGioia <davidofjoy@gmail.com>
2019-12-16 10:32:38 -0500

    sleep every frame to reach target FPS

--- AsciiRenderer/src/Main.cpp
+++ AsciiRenderer/src/Main.cpp
@@ -1,4 +1,6 @@
 #include <cstdio>
+#include <chrono>
+#include <thread>
 #include "Input.h"
 #include "Render.h"
 #include "Player.h"
@@ -6,6 +8,8 @@
 // width and height (in characters) of buffer
 constexpr unsigned WIDTH{ 80 };
 constexpr unsigned HEIGHT{ 20 };
+constexpr int FPS{ 30 };
+const std::chrono::milliseconds frameTime{ 1000 / FPS }; // 1000 bc we're using milliseconds
 
 // buf stores all the characters that will be drawn to screen
 Buffer buf{ WIDTH, HEIGHT };
@@ -14,11 +18,21 @@ Player player;
 
 int main()
 {
+
+
+
 	while (true)
 	{
+		auto start{ std::chrono::system_clock::now() };
+
 		playerInput(buf, player);
 		buf.clear();
 		buf.at(player.getRow(), player.getCol()) = 'X';
 		draw(buf);
+
+		// Sleep for number of milliseconds which puts us at our target FPS
+		auto end{ std::chrono::system_clock::now() };
+		auto elapsed{ std::chrono::duration_cast<std::chrono::milliseconds>(end - start) };
+		std::this_thread::sleep_for(frameTime - elapsed);
 	}
 }
